df9713a035e1e65397df699d627d20bcf4782a78
smargolies1 <sara@colorandcode.co>
2026-04-28 00:57:35 -0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-black">
+      <h1 className="text-6xl font-bold tracking-widest text-white uppercase">
+        Harvey Spector
+      </h1>
+    </div>
+  );
+}
